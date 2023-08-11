a84dae0a5e618bbc65ba7b97591aa39b0830af40
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-08-11 12:17:30 +0100

    indeed scraper added

--- src/Components/Results /PageNavigation.jsx
+++ src/Components/Results /PageNavigation.jsx
@@ -1,61 +1,31 @@
 import React from "react";
 import { useState } from "react";
+import Loader from "../Loader";
 
-const PageNavigation = ({ selectedPage, setSelectedPage }) => {
-  const [pageNumbers, setPageNumbers] = useState([1, 2, 3, 4, 5]);
-
-  // Function to increase pageNumbers by 5
-  const changePageNumbers = (direction) => {
-    setSelectedPage(pageNumbers[4] + 1);
-    let updatedPageNumbers = pageNumbers;
-    for (let i = 0; i < pageNumbers.length; i++) {
-      if (direction === "next") {
-        updatedPageNumbers[i] += 5;
-      } else if (updatedPageNumbers[4] >= 6) {
-        updatedPageNumbers[i] -= 5;
-      }
-    }
-    console.log(updatedPageNumbers);
-    setPageNumbers(updatedPageNumbers);
+const PageNavigation = ({
+  selectedPage,
+  setSelectedPage,
+  loaderActive,
+  setLoaderActive,
+}) => {
+  const handleClick = () => {
+    setSelectedPage(selectedPage + 1);
+    setLoaderActive(true);
   };
 
   return (
-    <nav aria-label="Page navigation">
-      <ul className="list-style-none flex font-bold text-lg">
-        <li>
-          <a
-            className="relative block rounded bg-transparent px-3 py-1.5  text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-            href="#"
-            onClick={() => changePageNumbers("previous")}
-          >
-            Previous
-          </a>
-        </li>
-        {pageNumbers.map((page, index) => (
-          <li>
-            <a
-              className={`relative block rounded bg-transparent px-3 py-1.5  text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white ${
-                selectedPage === page && `bg-neutral-600 text-white `
-              }`}
-              href="#"
-              onClick={() => setSelectedPage(page)}
-            >
-              {page}
-            </a>
-          </li>
-        ))}
-
-        <li>
-          <a
-            className="relative block rounded bg-transparent px-3 py-1.5  text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-            href="#"
-            onClick={() => changePageNumbers("next")}
-          >
-            Next
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {!loaderActive ? (
+        <button
+          className=" font-bold  p-2 border-2 border-black"
+          onClick={() => handleClick()}
+        >
+          See More +
+        </button>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 
